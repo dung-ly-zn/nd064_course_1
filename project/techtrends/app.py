f38ce03e522795db1c2f02fcfe6c72175463f358
dung.ly.zn <dung.ly.zn@renesas.com>
2024-06-14 09:16:33 +0700

    step1

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,13 +1,19 @@
 import sqlite3
+import logging
+import sys
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+
+# Connection counter
+connection_count = 0
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    connection_count += 1
     return connection
 
 # Function to get a post using its ID
@@ -36,13 +42,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.info('The 404 page retrieved!')
       return render_template('404.html'), 404
     else:
+      app.logger.info('Artical "' + post['title'] + '" retrieved!')
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('The "About Us" page retrieved!')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -60,11 +69,36 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-
+            app.logger.info('Artical "' + title + '" created!')
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
+# Define Healthcheck endpoint
+@app.route('/healthz')
+def healthz():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+    return response
+
+# Define Metrics endpoint
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    connection.close()
+    response = app.response_class(
+            response=json.dumps({"db_connection_count":connection_count,"post_count":posts.len()}),
+            status=200,
+            mimetype='application/json'
+    )
+    return response
+
 # start the application on port 3111
 if __name__ == "__main__":
+   # Stream logs to stdout at DEBUG level
+   logging.basicConfig(level=logging.DEBUG, handlers=logging.StreamHandler(sys.stdout))
    app.run(host='0.0.0.0', port='3111')
